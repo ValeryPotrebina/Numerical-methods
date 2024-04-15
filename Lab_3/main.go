9d0b0dc3929cc695212b50d97d34cc1ab77deb8f
ValeryPotrebina <valery_potrebina_2003@mail.ru>
2024-04-15 19:59:29 +0300

    fixed 3 lab

--- Lab_3/main.go
+++ Lab_3/main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func f(x float64) float64 {
-	return 0.9*x + 0.7
+	return -0.1027*math.Pow(x, 3) + 1.2349*math.Pow(x, 2) - 0.3256*x + 1.3851
 }
+
+//−0.1027x3+1.2349x2−0.3256x+1.3851
 
 func main() {
 	x := [...]float64{
@@ -16,15 +18,22 @@ func main() {
 	y := [...]float64{
 		2.35, 2.74, 5.37, 6.96, 8.52, 10.52, 13.41, 15.93, 17.61,
 	}
+	// Из учебника формулы
+	// ср арифметическое
 	xAr := (x[0] + x[8]) / 2
+	// ср геометрическое
 	xGeom := math.Sqrt(x[0] * x[8])
+	// ср гармоническое
 	xHarm := 2 / (1/x[0] + 1/x[8])
+
 	yAr := (y[0] + y[8]) / 2
 	yGeom := math.Sqrt(y[0] * y[8])
 	yHarm := 2 / (1/y[0] + 1/y[8])
+
 	zAr := f(xAr)
 	zGeom := f(xGeom)
 	zHarm := f(xHarm)
+
 	fmt.Println("X : ", xAr, xGeom, xHarm)
 	fmt.Println("Y : ", yAr, yGeom, yHarm)
 	fmt.Println("Z : ", zAr, zGeom, zHarm)
@@ -67,9 +76,10 @@ func main() {
 	fmt.Println("b = ", b)
 	var z [9]float64
 	for i, xx := range x {
-		z[i] = a*math.Log(xx) + b
+		// z6
+		z[i] = xx / (a*xx + b)
 	}
-	fmt.Println(z)
+	fmt.Println("z = ", z)
 	del := 0.0
 	for i := 0; i < n-1; i++ {
 		del += (z[i] - y[i]) * (z[i] - y[i]) / float64(n)
@@ -77,3 +87,5 @@ func main() {
 	del = math.Sqrt(del)
 	fmt.Println(" del = ", del)
 }
+
+// итог z6
